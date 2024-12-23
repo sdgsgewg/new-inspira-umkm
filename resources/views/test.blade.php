8ef60bf84593342f743c4aa0f64eef784553c43f
Jessen <jessen123ptk@gmail.com>
2024-12-23 18:18:51 +0700

    coba2

---
+++ resources/views/test.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Test Deploy</title>
+    <link rel="stylesheet" href={{ asset('build/assets/app.css') }}>
+</head>
+
+<body>
+    <div class="box">
+        <h1>Test Deploy</h1>
+        <h2>Saye Upin</h2>
+        <h2>Ini Adik Saya Ipin</h2>
+        <h3>Ini Kisah Kami Semue</h3>
+    </div>
+</body>
+
+</html>
