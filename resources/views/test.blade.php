cba7f2c51e0762b767be13439817e09106e6d444
Jessen <jessen123ptk@gmail.com>
2024-12-23 19:31:57 +0700

    stff

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Test Deploy</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     {{-- <link rel="stylesheet" href="../css/app.css"> --}}
 </head>
 
