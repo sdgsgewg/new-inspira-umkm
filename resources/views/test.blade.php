027f494f14c42e6f0e3eae0f4aeeb3d63ad20f5d
Jessen <jessen123ptk@gmail.com>
2024-12-23 18:26:41 +0700

    cob

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Test Deploy</title>
-    <link rel="stylesheet" href={{ asset('build/assets/app.css') }}>
+    <link rel="stylesheet" href="{{ asset('app.css') }}?v={{ time() }}">
 </head>
 
 <body>
